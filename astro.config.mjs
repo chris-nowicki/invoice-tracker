bd2fa753a93d9148fda260055482f710fca3640c
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2026-02-22 14:59:50 -0500

    add astro vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-import node from "@astrojs/node";
 
 import tailwindcss from "@tailwindcss/vite";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  adapter: node({ mode: "standalone" }),
+  adapter: vercel(),
+  output: "server",
 
   server: {
     allowedHosts: true,
